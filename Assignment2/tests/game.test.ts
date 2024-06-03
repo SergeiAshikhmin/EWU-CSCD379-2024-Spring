9074babb29b487591e12a21660b51253a7a4cf18
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-06-03 16:45:44 -0700

    refactor: Remove all files from Assignment

--- Assignment2/tests/game.test.ts
+++
@@ -1,9 +0,0 @@
-// @vitest-environment nuxt
-import { expect, test } from "vitest";
-import { Game } from "~/scripts/game";
-import { LetterState } from "~/scripts/letter";
-
-test("game", () => {
-  // create game and check if it's created
-
-});
--- README.md
+++
@@ -1,3 +0,0 @@
-Assignment1:
-
-Azure link: https://lemon-flower-0c0aec71e.5.azurestaticapps.net
